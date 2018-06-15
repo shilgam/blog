b6dd0d5ecc82861d9e3d9985531208f12c039f48
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-06-15 13:26:55 +0300

    Integration tests

--- test/integration/blog_flow_test.rb
+++ test/integration/blog_flow_test.rb
@@ -1,7 +1,22 @@
-require 'test_helper'
+require "test_helper"
 
 class BlogFlowTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can see the welcome page" do
+    get "/"
+    assert_select "h1", "Hello, Rails!"
+  end
+
+  test "can create an article" do
+    get "/articles/new"
+    assert_response :success
+
+    post "/articles",
+      params: { article: { title: "can create", text: "article successfully" } }
+
+    assert_response :redirect
+    follow_redirect!
+    # byebug
+    assert_response :success
+    assert_select "p", "Title:\n  can create"
+  end
 end
